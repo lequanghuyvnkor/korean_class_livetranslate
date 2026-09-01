d8649efd4ddefc10954fc8e3f1673bcedd67cd39
lequanghuyvnkor <lequanghuy8102005@snu.ac.kr>
2026-09-01 18:35:46 +0900

    clean: remove temp test files

--- test_sd.py
+++
@@ -1,7 +0,0 @@
-import sounddevice as sd
-
-print("=== SoundDevice Input Devices ===")
-devs = sd.query_devices()
-for idx, dev in enumerate(devs):
-    if dev['max_input_channels'] > 0:
-        print(f"ID: {idx} | Name: {dev['name']} | Channels: {dev['max_input_channels']}")
